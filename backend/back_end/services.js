965162b1a01a6b81cfc2ebf5db1a924447662c7d
Angela Beatrice Masayes <angelabroncanomasayes@gmail.com>
2025-04-13 10:38:34 +0800

    backend update

--- backend/back_end/services.js
+++ backend/back_end/services.js
@@ -1,6 +1,7 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
+console.log("MySQL User:", process.env.MYSQL_USER);
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
