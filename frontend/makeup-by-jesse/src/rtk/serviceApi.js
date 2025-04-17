f72e5460a27976b2122af356dbdfd5bdd5458bfe
princejimweldin <princejimweldinxme@gmail.com>
2025-04-17 16:28:38 +0800

    connect

--- frontend/makeup-by-jesse/src/rtk/serviceApi.js
+++ frontend/makeup-by-jesse/src/rtk/serviceApi.js
@@ -3,14 +3,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const serviceApi = createApi({
   reducerPath: 'serviceApi',
   // Make sure this matches your backend URL
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }), 
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }), 
   tagTypes: ['Services'],
   endpoints: (builder) => ({
-    // Get all services
-    getServices: builder.query({
-      query: () => '/services',
-      providesTags: ['Services'],
-    }),
     // Add a service
     addService: builder.mutation({
       query: (newService) => ({
@@ -20,29 +15,10 @@ export const serviceApi = createApi({
       }),
       invalidatesTags: ['Services'],
     }),
-    // Update a service
-    updateService: builder.mutation({
-      query: ({ id, ...service }) => ({
-        url: `/services/${id}`,
-        method: 'PUT',
-        body: service,
-      }),
-      invalidatesTags: ['Services'],
-    }),
-    // Delete a service
-    deleteService: builder.mutation({
-      query: (id) => ({
-        url: `/services/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['Services'],
-    }),
   }),
 });
 
 export const { 
   useGetServicesQuery, 
   useAddServiceMutation, 
-  useUpdateServiceMutation,
-  useDeleteServiceMutation
 } = serviceApi;
