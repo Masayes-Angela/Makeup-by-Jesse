d84ac0e96d88fd33ae34c4b5c3c284a494c92afe
princejimweldin <princejimweldinxme@gmail.com>
2025-05-19 19:03:20 +0800

    change port

--- backend/app.js
+++ backend/app.js
@@ -79,7 +79,7 @@ app.delete("/services/:id", async (req, res) => {
   }
 })
 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
