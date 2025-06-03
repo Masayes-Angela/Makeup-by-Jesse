adf5f9096adea6749d1a46a91c6636192622edb7
Sofia Ladylyn Mercado <sofia1809.mercado@gmail.com>
2025-06-03 23:31:48 +0800

    frontend admin sidebar upd

--- frontend/makeup-by-jesse/src/app/admin/components/Sidebar.js
+++ frontend/makeup-by-jesse/src/app/admin/components/Sidebar.js
@@ -3,7 +3,12 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styles from './Sidebar.module.css';
-import { FaTachometerAlt, FaBookmark, FaCalendarAlt, FaEdit, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import { FaRegCalendar, FaSignOutAlt } from 'react-icons/fa';
+import { HiOutlineBookmark } from "react-icons/hi";
+import { LuPencilLine } from "react-icons/lu";
+import { FiSettings } from "react-icons/fi";
+import { RxDashboard } from "react-icons/rx";
+import { TbLogout } from "react-icons/tb";
 
 export default function Sidebar() {
   const pathname = usePathname();
@@ -16,33 +21,33 @@ export default function Sidebar() {
       <ul className={styles['sidebar-menu']}>
         <li className={pathname === '/admin/dashboard' ? styles.active : ''}>
           <Link href="/admin/dashboard">
-            <FaTachometerAlt className={styles.icon}/> Dashboard
+            <RxDashboard className={styles.icon}/> Dashboard
           </Link>
         </li>
         <li className={pathname === '/admin/bookings' ? styles.active : ''}>
           <Link href="/admin/bookings">
-            <FaBookmark className={styles.icon}/> Manage Bookings
+            <HiOutlineBookmark className={styles.icon}/> Manage Bookings
           </Link>
         </li>
         <li className={pathname === '/admin/schedule' ? styles.active : ''}>
           <Link href="/admin/schedule">
-            <FaCalendarAlt className={styles.icon}/> Schedule
+            <FaRegCalendar className={styles.icon}/> Schedule
           </Link>
         </li>
         <li className={pathname === '/admin/content' ? styles.active : ''}>
           <Link href="/admin/content">
-            <FaEdit className={styles.icon}/> Manage Website Content
+            <LuPencilLine className={styles.icon}/> Manage Website Content
           </Link>
         </li>
         <li className={pathname === '/admin/settings' ? styles.active : ''}>
           <Link href="/admin/settings">
-            <FaCog className={styles.icon}/> Settings
+            <FiSettings className={styles.icon}/> Settings
           </Link>
         </li>
       </ul>
       <div className={styles['sidebar-footer']}>
         <Link href="/admin/logout">
-          <FaSignOutAlt className={styles['logout-icon']}/> Logout
+          <TbLogout className={styles['logout-icon']}/> Logout
         </Link>
       </div>
     </div>
