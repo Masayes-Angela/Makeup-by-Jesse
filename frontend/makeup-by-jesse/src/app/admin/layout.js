d0a8369af3b5671213a7a1da17ad778443fa8070
Sofia Ladylyn Mercado <sofia1809.mercado@gmail.com>
2025-06-05 03:46:05 +0800

    frontend admin bookings, removed schedule folder

--- frontend/makeup-by-jesse/src/app/admin/layout.js
+++ frontend/makeup-by-jesse/src/app/admin/layout.js
@@ -1,17 +1,28 @@
 // app/admin/layout.js
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Sidebar from '@/app/admin/components/Sidebar';
 import { Provider } from 'react-redux';
 import { store } from '@/rtk/store';
 import '../globals.css';
 
 export default function AdminLayout({ children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('adminToken');
+    if (!token) {
+      router.push('/auth/login'); // updated route
+    }
+  }, []);
+
   return (
     <html lang="en">
       <body>
         <Provider store={store}>
-          <div className="app" style={{ backgroundColor: '#f9fcff' }}>
+          <div className="app" style={{ backgroundColor: '#F6FAFF' }}>
             <Sidebar />
             <main className="main-content">{children}</main>
           </div>
